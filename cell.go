4fa28f8f6bbc7b4d0010942c9edfc7ee166924e7
Alex Gonopolskiy <agonopol@gmail.com>
2011-09-17 18:20:01 -0400

    shie

--- cell.go
+++ cell.go
@@ -1,5 +1,9 @@
 package strugglin
 
+import "math"
+import "fmt"
+
+
 
 type CellState struct {
   occupied bool
@@ -23,6 +27,49 @@ func (this *Cell) Occupied() bool {
   return this.state.occupied
 }
 
+
+func Abs(n int32) int32{
+  if n < 0 { 
+     return -n 
+  } 
+  return n
+}
+func (this *Cell) AddNeighbor(neighbor *Cell) {
+    var x,y int32
+    if Abs(neighbor.X) > Abs(this.X) {
+      x = neighbor.X - this.X
+    } else {
+      x = this.X - neighbor.X
+    }
+    if Abs(neighbor.Y) > Abs(this.Y) {
+       y = neighbor.Y - this.Y
+    }  else {
+       y = this.Y - neighbor.Y
+    }
+    _,found := this.neighbors[x]
+    if !found {
+      this.neighbors[x] = make(map[int32] *Cell)
+    } 
+    println(fmt.Sprintf("[%v][%v].neighbors[%v][%v] = [%v][%v]", this.X, this.Y, x, y, neighbor.X,neighbor.Y))
+    this.neighbors[x][y] = neighbor
+}
+
+func (this *Cell) notifyNeighors(cell * Cell) {
+  for _,xplane := range this.neighbors {
+    for _,neighbor := range xplane {
+      d := neighbor.Distance(cell) 
+      if d < 2.0 {
+        neighbor.AddNeighbor(cell)
+      }
+    }
+  }
+}
+
+func (this *Cell) Distance(other * Cell) float64 {
+ return math.Sqrt(math.Pow(float64(this.X - other.X),2) + math.Pow(float64(this.Y - other.Y),2))
+}
+
+
 func (this *Cell) Neighbor(x,y int32) *Cell {
   _,found := this.neighbors[x]
   if !found {
@@ -30,7 +77,8 @@ func (this *Cell) Neighbor(x,y int32) *Cell {
   }
   _,found = this.neighbors[x][y]
   if !found {
-   this.neighbors[x][y] = NewCell(this.X + x, this.Y + y)
+   cell := NewCell(this.X + x, this.Y + y)
+   this.notifyNeighors(cell)
   }
   return this.neighbors[x][y]
 }
---
+++ cell_test.go
@@ -0,0 +1,22 @@
+package strugglin
+
+import "testing"
+
+func compare(t *testing.T, expected, actual interface{}) {
+	if expected != actual {
+		t.Errorf("value differs. Expected [%v], actual [%v]", expected, actual)
+	}
+}
+
+
+func TestDistnace(t *testing.T) {
+  one := NewCell(0,0)
+  other := NewCell(0,1)
+  compare(t, one.Distance(other), 1.0)
+  one = NewCell(-1,-1)
+  other = NewCell(0,1)
+  compare(t, one.Distance(other), 2.23606797749979)
+  one = NewCell(1,1)
+  other = NewCell(0,0)
+  compare(t, one.Distance(other), 1.4142135623730951)
+} 
