541833115a6e4cbce502c2a20468ba86a6db756f
Lee Aylward <lee@laylward.com>
2011-09-17 19:18:36 -0400

    begin drawing grid on the canvas

--- public/strugglin.js
+++ public/strugglin.js
@@ -1,9 +1,40 @@
-var Game = Class.create({
+var World = Class.create({
+
   initialize: function(id) {
     this.canvas = $(id);
+    this.canvas.width = document.viewport.getWidth();
+    this.canvas.height = document.viewport.getHeight();
+
+    this.context = this.canvas.getContext("2d");
+    this.gridSize = 20; // length of a grid side
+    this.drawGrid();
+  },
+
+  drawGrid: function() {
+    var ctx = this.context;
+    var width = this.canvas.width;
+    var height = this.canvas.height;
+    var x = 0, y = 0;
+
+    while (x <= width) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+      ctx.stroke();
+      x += this.gridSize;
+    }
+
+    while (y <= height) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.moveTo(width, y);
+      ctx.stroke();
+      y += this.gridSize;
+    }
   }
+
 });
 
 document.observe("dom:loaded", function() {
-  var game = new Game("game");  
+  var game = new World("view");  
 });
